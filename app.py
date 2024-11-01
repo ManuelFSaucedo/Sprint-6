fef2ecfb590c60c2528a9b84902b882fc154a34c
ManuelFSaucedo <manuelfsv20@gmail.com>
2024-11-01 01:15:08 -0600

    actualizacion de importaciones y borrado de vehicles.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import plotly
-import plotly_express as px
+import plotly.express as px
 import streamlit as st
-import numpy as np
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 st.header('Vehicles')
